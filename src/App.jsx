a6924718a63f46321da50bd2762528a973108525
Ayush H <jqghebri@gmail.com>
2024-08-28 18:01:29 +0530

    creation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [ loading, setLoading] = useState(true)
-  const dispatch = useDispatch(true)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     authService.getCurrentUser()
@@ -22,7 +24,13 @@ function App() {
 
   return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      test
+      <div className='w-full block'>
+    <Header />
+    <main>
+      TODO: <Outlet />
+    </main>
+    <Footer />
+      </div>
     </div>
   ) : (null)
 }
