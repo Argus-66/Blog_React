4a4269f5daf65ed2f5f752505ab97969a1102143
Ayush H <jqghebri@gmail.com>
2024-08-29 18:27:25 +0530

    creation

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -9,10 +9,11 @@ function LogoutBtn() {
         authService.logout().then(() => {
             dispatch(logout())
         })
-
     }
   return (
-    <div>LogoutBtn</div>
+    <button
+    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounde-full'
+    >Logoput</button>
   )
 }
 
