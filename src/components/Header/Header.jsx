83c2c35f7df3d0567349773494c51bcea12cb19f
Ayush H <jqghebri@gmail.com>
2024-08-29 19:25:07 +0530

    creation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -52,9 +52,15 @@ function Header() {
                 <li key={item.name}>
                   <button
                   onClick={() => navigate(item.slug)}
+                  className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
                   >{item.name}</button>
                 </li>
               ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
             )}
           </ul>
         </nav>
--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -13,7 +13,7 @@ function LogoutBtn() {
   return (
     <button
     className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounde-full'
-    >Logoput</button>
+    >Logout</button>
   )
 }
 
