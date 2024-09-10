edfda25ad0a0ae788ec53e0f4513b824eda63a31
Ayush H <jqghebri@gmail.com>
2024-09-10 15:10:11 +0530

    creation

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -9,8 +9,23 @@ function Select({
     const id = useId()
     
   return (
-    <div className='w-full'>Select</div>
+    <div className='w-full'>
+        {label && <label htmlFor={id} className=''></label>}
+        <select 
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        >
+            {options?.map((option) => (
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            ))}
+
+        </select>
+        </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
