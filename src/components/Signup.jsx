3a74e10535353e06993e3b385ae51fb3d1fc59f6
Ayush H <jqghebri@gmail.com>
2024-09-12 13:30:33 +0530

    creation

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,9 +2,25 @@ import { useState } from "react"
 import React from 'react'
 import authService from "../appwrite/auth"
 import {Link, useNavigate} from 'react-router-dom'
-
+import {Login} from '../store/authSlice'
+import {Button, Input, Logo} from './index'
+import { useDispatch } from "react-redux"
+import { useForm } from "react-hook-form"
 
 function Signup() {
+    const navigate = useNavigate()
+    const [error, setError] = useState("")
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+
+    const create = async(data) => {
+        setError("")
+        try {
+            
+        } catch (error) {
+            setError(error.message)
+        }
+    }
   return (
     <div>Signup</div>
   )
