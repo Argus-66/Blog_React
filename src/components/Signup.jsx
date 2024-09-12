3b6892458febfaeb06ba9011fb84e5756b7fb2a0
Ayush H <jqghebri@gmail.com>
2024-09-12 13:26:46 +0530

    creation

---
+++ src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import React from 'react'
+import authService from "../appwrite/auth"
+import {Link, useNavigate} from 'react-router-dom'
+
+
+function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
