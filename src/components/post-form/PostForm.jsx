f77500a54a7ffa322ce1dca391691d0fbc6b6a20
Ayush H <jqghebri@gmail.com>
2024-09-14 23:42:36 +0530

    creation

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -18,7 +18,38 @@ function PostForm(post) {
 
     const navigate = useNavigate()
     const userData = useSelector(state => state.user.UserData)
-    
+
+    const submit = async (data) => {
+        if (post) {
+            const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
+
+            if (file) {
+                appwriteService.deleteFile(post.featuredImage);
+            }
+
+            const dbPost = await appwriteService.updatePost(post.$id, {
+                ...data,
+                featuredImage: file ? file.$id : undefined,
+            });
+
+            if (dbPost) {
+                navigate(`/post/${dbPost.$id}`);
+            }
+        } else {
+            const file = await appwriteService.uploadFile(data.image[0]);
+
+            if (file) {
+                const fileId = file.$id;
+                data.featuredImage = fileId;
+                const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id });
+
+                if (dbPost) {
+                    navigate(`/post/${dbPost.$id}`);
+                }
+            }
+        }
+    };
+
     return (
     <div>PostForm</div>
     )
