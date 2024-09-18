d724a1f52ef7ccd8802fe271873bd21adf08d100
Ayush H <jqghebri@gmail.com>
2024-09-18 17:17:28 +0530

    creation

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
 
 import AddPost from "./pages/AddPost";
