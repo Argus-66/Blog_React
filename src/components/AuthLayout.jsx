bf2b1512464f9416eb2c2793db276d34c43f187d
Ayush H <jqghebri@gmail.com>
2024-09-18 17:31:51 +0530

    creation

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -4,9 +4,9 @@ import { useNavigate } from 'react-router-dom'
 
 export default function Protected({children, authentication = true, }) {
 
-    const navigate = useNavigate
+    const navigate = useNavigate()
     const [loader, setLoader] = useState(true)
-    const authStatus = useSelector(state.auth.status)
+    const authStatus = useSelector(state => state.auth.status)
 
     useEffect(() => {
         //true && true
