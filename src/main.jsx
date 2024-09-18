a287f985aa32659493c6422acbb018db00623b59
Ayush H <jqghebri@gmail.com>
2024-09-18 16:56:59 +0530

    creation

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,27 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store'
+import { createBrowserRouter } from 'react-router-dom'
+import { AuthLayout } from './components/index.js'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <app /,
+    children:[
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout
+        ),
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
