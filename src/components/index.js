2b4d6ad9e95ca6ab8e3d7c1be26d2e324cce3db6
Ayush H <jqghebri@gmail.com>
2024-09-18 16:50:11 +0530

    creation

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,19 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./Container/Container";
+import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
 import Button from "./Button";
-import Post from "./Post";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 
-export{
+export {
     Header,
     Footer,
     Container,
@@ -21,8 +23,9 @@ export{
     Signup,
     Login,
     Button,
-    Post,
     PostForm,
     PostCard,
-
+    AuthLayout, 
+    Input,
+    Select,
 }
